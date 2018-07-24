8cdbb53ef2cbf645d4768950d30bb764030628e6
Emmanuel <emmanuel.barbin@gmail.com>
2018-07-24 15:35:48 -0300

    proxy config and test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TestService } from './test.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private testService: TestService) {}
+
+  onTest() {
+    this.testService.test();
+  }
 }
