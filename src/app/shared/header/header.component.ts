e5bd9258fe92f8a06f3dbc2c01b8962bcbff74c7
Emmanuel <emmanuel.barbin@gmail.com>
2018-08-17 21:39:16 -0300

    fix

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,32 +1,38 @@
 import { UserService } from './../../user/user.service';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../../user/user.model';
+import { Subscription } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  isCollapsed: boolean;
   constructor(private httpClient: HttpClient, private userService: UserService,
      private router: Router, private toastService: ToastrService) { }
 
+  isCollapsed: boolean;
   username: string;
   imageId: string;
+  subs: Subscription;
 
   ngOnInit() {
     const user: User = this.userService.getStorageUser();
     this.username = user.username;
     this.imageId = user.imageId;
 
-    this.userService.imageUpdated.subscribe((imageId: string) => {
+    this.subs = this.userService.imageUpdated.subscribe((imageId: string) => {
       this.imageId = imageId;
     });
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
   logout() {
