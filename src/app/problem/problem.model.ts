93933d2819868f4b3af2bda1a6ca4d61e9f55987
Emmanuel <emmanuel.barbin@gmail.com>
2018-08-19 19:43:26 -0300

    request data holder

--- src/app/problem/problem.model.ts
+++ src/app/problem/problem.model.ts
@@ -1,3 +1,3 @@
 export class Problem {
-  constructor(public id: string, public name: string) {}
+  constructor(public id: string, public name: string, public instituteId: string, public userId: string) {}
 }
--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -1,5 +1,6 @@
 export class User {
   constructor(public username: string, public password: string,
     public name: string, public surename: string, public documentType: string,
-    public documentNumber: string, public roles: [string], public lastEvent: {token: string}, public imageId: string) {}
+    public documentNumber: string, public roles: [string], public lastEvent: {token: string},
+    public imageId: string, public instituteId: string) {}
 }
