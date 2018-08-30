0fcd7a6e555a24bee96f04cb6e90c059145cc2ee
Emmanuel <emmanuel.barbin@gmail.com>
2018-08-30 09:39:56 -0300

    ajuste de layout. problem pre y pos execution

--- src/app/problem/problem.model.ts
+++ src/app/problem/problem.model.ts
@@ -2,5 +2,5 @@ import { Primitive } from './../primitive/primitive.model';
 export class Problem {
   constructor(public id: string, public name: string, public instituteId: string,
     public teacher: string, public explanation: string, public solution: string,
-    public primitives: [Primitive], public context: string) {}
+    public primitives: [Primitive], public preExecution: string, public posExecution: string) {}
 }
