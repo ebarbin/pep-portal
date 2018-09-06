81105b83dd6685c4d66a61c94364550fd8ce631c
Emmanuel <emmanuel.barbin@gmail.com>
2018-09-06 09:30:36 -0300

    ajustes

--- src/app/dialog/help-dialog/help-dialog.component.ts
+++ src/app/dialog/help-dialog/help-dialog.component.ts
@@ -9,11 +9,38 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HelpDialogComponent implements OnInit {
 
   @Input() url: string;
+  title: string;
 
   constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit() {
-
+    if (this.url.includes('/home/start')) {
+      this.title = 'Bienvenida';
+    } else if (this.url.includes('/home/workspace')) {
+      this.title = 'Area de Trabajo';
+    } else if (this.url.includes('/home/course/list')) {
+      this.title = 'Mis Cursos';
+    } else if (this.url.includes('/home/student-consultations')) {
+      this.title = 'Mis Consultas';
+    } else if (this.url.includes('/home/account')) {
+      this.title = 'Cuenta';
+    } else if (this.url.includes('/home/change-password')) {
+      this.title = 'Cambio de Contraseña';
+    } else if (this.url.includes('/home/profile-image')) {
+      this.title = 'Imagen de Perfil';
+    } else if (this.url.includes('/home/problem/list')) {
+      this.title = 'Mis Ejercicios';
+    } else if (this.url.includes('/home/new-problem')) {
+      this.title = 'Crear Ejercicios';
+    } else if (this.url.includes('/home/edit-problem')) {
+      this.title = 'Editar Ejercicio';
+    } else if (this.url.includes('/home/primitive/list')) {
+      this.title = 'Mis Primitivas';
+    } else if (this.url.includes('/home/new-primitive')) {
+      this.title = 'Crear Primitiva';
+    } else if (this.url.includes('/home/edit-primitive')) {
+      this.title = 'Editar Primitiva';
+    }
   }
 
   public close() {
